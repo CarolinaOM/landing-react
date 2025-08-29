5599ad65874d621a46d13cbd73aa2a2ea867eb88
CarolinaOM <caro13081991@gmail.com>
2025-08-29 22:24:56 +0100

    Definiendo estilo de navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -27,29 +27,25 @@ const navbarlinks =[
 const Navbar = () => {
   return (
     <nav>
-      <div>
-
+      <div className='flex justify-between items-center sm:px-12 sm:py-6 px-4 py-3'>
+         
+         { /* Logo Navar */}
         <div>
             <img src={Logo} alt= 'Logo del sitio' className='w-[100px]' />
         </div>
 
+         { /* Navegación desktop */}
         <div>
-            <ul>
+            <ul className='flex sm:space-x-8 space-x-4'>
                 {navbarlinks.map((link)=>(
                     <li key={link.id}>
-                        <a href={link.link}> {link.title} </a>
+                        <a className='text-white sm:text-lg text-sm hover:text-sky-200 
+                        transition-trasform hover:scale-110 transform inline-block duration-300' 
+                        href={link.link}> {link.title} </a>
                     </li>
                 ))}
             </ul>
         </div>
-
-        <div>
-            <ul>
-                <li>Redes</li>
-                <li>Redes</li>
-            </ul>
-        </div>
-
       </div>
     </nav>
   )
