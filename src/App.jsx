708dcdde316d33779c41df1dfb0d43d1fc75eaa5
CarolinaOM <caro13081991@gmail.com>
2025-08-29 18:39:15 +0100

    agregando iconos

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ function App() {
   
   return (
     <>
-      <h1 className="text-3x1 font-bold underline">
-        Hello world!
-      </h1>
+      <h1 className="text-3x1 font-bold underline">Hello world!</h1>
+      <i className="bi bi-1-circle-fill"></i>
     </>
   )
 }
