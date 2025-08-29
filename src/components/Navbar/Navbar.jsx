cb5f894b4dfa52aaf1f4c18ab73c516879d00f6b
CarolinaOM <caro13081991@gmail.com>
2025-08-29 21:21:06 +0100

    Definiendo navegacion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import Logo from '../../assets/Logo.png'
+
+const navbarlinks =[
+    {
+        id:1,
+        title:"Inicio",
+        link:"/"
+    },
+    {
+        id:2,
+        title:"Nosotros",
+        link:"#"
+    },
+    {
+        id:3,
+        title:"Contacto",
+        link:"#"
+    },
+    {
+        id:4,
+        title:"Soporte",
+        link:"#"
+    },
+]
 
 const Navbar = () => {
   return (
     <nav>
-      navbar
+      <div>
+
+        <div>
+            <img src={Logo} alt= 'Logo del sitio' className='w-[100px]' />
+        </div>
+
+        <div>
+            <ul>
+                {navbarlinks.map((link)=>(
+                    <li key={link.id}>
+                        <a href={link.link}> {link.title} </a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+
+        <div>
+            <ul>
+                <li>Redes</li>
+                <li>Redes</li>
+            </ul>
+        </div>
+
+      </div>
     </nav>
   )
 }
